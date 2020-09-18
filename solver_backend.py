83839d5b58d919f3d31d6149aee502b9d79bb52d
Stuler <peter.jurek@gmail.com>
2020-09-18 22:05:09 +0200

    Deck created

---
+++ solver_backend.py
@@ -0,0 +1,35 @@
+cols, rows = (9,9)
+deck = [[0 for i in range(cols)]for j in range(rows)]
+
+deck[0][0] = 5
+deck[0][1] = 3
+deck[0][4] = 7
+deck[1][0] = 6
+deck[1][3] = 1
+deck[1][4] = 9
+deck[1][5] = 5
+deck[2][1] = 9
+deck[2][2] = 8
+deck[2][7] = 6
+deck[3][0] = 8
+deck[3][4] = 6
+deck[3][8] = 3
+deck[4][0] = 4
+deck[4][3] = 8
+deck[4][5] = 3
+deck[4][8] = 1
+deck[5][0] = 7
+deck[5][4] = 2
+deck[5][8] = 6
+deck[6][1] = 6
+deck[6][6] = 2
+deck[6][7] = 8
+deck[7][3] = 4
+deck[7][4] = 1
+deck[7][5] = 9
+deck[7][8] = 5
+deck[8][4] = 8
+deck[8][7] = 7
+deck[8][8] = 9
+
+print(deck)
