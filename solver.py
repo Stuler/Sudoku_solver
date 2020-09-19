88c1c1a4db9ab6fdeb1128d921a974f081bb9820
Stuler <peter.jurek@gmail.com>
2020-09-19 21:30:39 +0200

    Little update

--- solver.py
+++ solver.py
@@ -1,58 +1,61 @@
 from deck import deck
 
-available_numbers = []
-row_availables = []
-final_deck = []
+def get_col(lst, pos):
+    return(lst.index(pos))
 
-sect_1 = [
+available_numbers = []
+final_deck = []
+temp_deck = []
+
+sects = [
+    [# section 1 index 0
     deck[0][0],deck[0][1],deck[0][2],
     deck[1][0],deck[1][1],deck[1][2],
-    deck[2][0],deck[2][1],deck[2][2]]
-
-sect_2 = [
+    deck[2][0],deck[2][1],deck[2][2]],
+    [# section 2 index 1
     deck[0][3],deck[0][4],deck[0][5],
     deck[1][3],deck[1][4],deck[1][5],
-    deck[2][3],deck[2][4],deck[2][5]]
-
-sect_3 = [
+    deck[2][3],deck[2][4],deck[2][5]],
+    [# section 3 index 2
     deck[0][6],deck[0][7],deck[0][8],
     deck[1][6],deck[1][7],deck[1][8],
-    deck[2][6],deck[2][7],deck[2][8]]
-
-sect_4 = [
+    deck[2][6],deck[2][7],deck[2][8]],
+    [# section 4 index 3
     deck[3][0],deck[3][1],deck[3][2],
     deck[4][0],deck[4][1],deck[4][2],
-    deck[5][0],deck[5][1],deck[5][2]]
-
-sect_5 = [
+    deck[5][0],deck[5][1],deck[5][2]],
+    [# section 5 index 4
     deck[3][3],deck[3][4],deck[3][5],
     deck[4][3],deck[4][4],deck[4][5],
-    deck[5][3],deck[5][4],deck[5][5]]
-
-sect_6 = [
+    deck[5][3],deck[5][4],deck[5][5]],
+    [# section 6 index 5
     deck[3][6],deck[3][7],deck[3][8],
     deck[4][6],deck[4][7],deck[4][8],
-    deck[5][6],deck[5][7],deck[5][8]]
-
-sect_7 = [
+    deck[5][6],deck[5][7],deck[5][8]],
+    [# section 7 index 6
     deck[6][0],deck[6][1],deck[6][2],
     deck[7][0],deck[7][1],deck[7][2],
-    deck[8][0],deck[8][1],deck[8][2]]
-
-sect_8 = [
+    deck[8][0],deck[8][1],deck[8][2]],
+    [# section 8 index 7
     deck[6][3],deck[6][4],deck[6][5],
     deck[7][3],deck[7][4],deck[7][5],
-    deck[8][3],deck[8][4],deck[8][5]]
-
-sect_9 = [
+    deck[8][3],deck[8][4],deck[8][5]],
+    [# section 9 index 8
     deck[6][6],deck[6][7],deck[6][8],
     deck[7][6],deck[7][7],deck[7][8],
     deck[8][6],deck[8][7],deck[8][8]]
-
+    ]
 
 for j in range(1,10):
+    temp_deck = deck
     for row in deck:
-        if j not in row:
-            row_availables.append(j)
+        for i in range(len(row)):
+            column = row.index(i)
+            if (j not in row) or (j not in (row[column]) for row in deck) and (i == 0):
+                temp_deck.insert(column,j)
+            else:
+                continue
+            #if j not in row: # check of missing numbers
+            #    available_numbers.append(j)
 
-print(row_availables)
+print(temp_deck)
---
+++ test.py
@@ -0,0 +1,40 @@
+cols, rows = (5,5)
+
+table = [[0 for i in range(cols)]for j in range(rows)]
+
+# row 1
+table[0][0]= "00"
+table[0][1]= "01"
+table[0][2]= "02"
+table[0][3]= "03"
+table[0][4]= "04"
+
+# row 2
+table[1][0]= "10"
+table[1][1]= "11"
+table[1][2]= "12"
+table[1][3]= "13"
+table[1][4]= "14"
+
+# row 3
+table[2][0]= "20"
+table[2][1]= "21"
+table[2][2]= "22"
+table[2][3]= "23"
+table[2][4]= "24"
+
+# row 4
+table[3][0]= "30"
+table[3][1]= "31"
+table[3][2]= "32"
+table[3][3]= "33"
+table[3][4]= "34"
+
+# row 5
+table[4][0]= "40"
+table[4][1]= "41"
+table[4][2]= "42"
+table[4][3]= "43"
+table[4][4]= "44"
+
+print(table)
